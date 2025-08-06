036ced46065043d0a45812481d8e113052ef86f3
yoinerdeyhan@gmail.com <yoinerdeyhan@gmail.com>
2025-08-06 13:44:01 -0500

    ✨ [feat] arreglo de rutas

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\Api\MarcacionController;
 
 // Rutas con código de empresa (multi-tenancy)
 Route::group(['prefix' => '{codEmpresa}', 'where' => ['codEmpresa' => '[0-9]+'], 'middleware' => 'empresa'], function () {
-    
+
     // Rutas para dispositivos ZKTeco con empresa
     Route::prefix('iclock')->group(function () {
         Route::get('/cdata', [UploadProcessController::class, 'getCdata']);
@@ -23,28 +23,6 @@ Route::group(['prefix' => '{codEmpresa}', 'where' => ['codEmpresa' => '[0-9]+'],
         Route::post('/devicecmd', [DownloadProcessController::class, 'postDeviceCmd']);
     });
 
-    // Rutas para marcaciones de aplicaciones móviles con empresa
-    Route::prefix('marcacion')->group(function () {
-        Route::post('/procesar', [MarcacionController::class, 'procesarMarcacion']);
-        Route::get('/obtener', [MarcacionController::class, 'obtenerMarcaciones']);
-        Route::get('/estado', [MarcacionController::class, 'verificarEstado']);
-        Route::get('/empresa', [MarcacionController::class, 'obtenerEmpresaInfo']);
-    });
-});
-
-// Rutas sin código de empresa (para compatibilidad)
-Route::prefix('iclock')->group(function () {
-    Route::get('/cdata', [UploadProcessController::class, 'getCdata']);
-    Route::post('/cdata', [UploadProcessController::class, 'postCdata']);
-    Route::get('/devicecmd', [DownloadProcessController::class, 'getRequest']);
-    Route::post('/devicecmd', [DownloadProcessController::class, 'postDeviceCmd']);
-});
-
-Route::prefix('marcacion')->group(function () {
-    Route::post('/procesar', [MarcacionController::class, 'procesarMarcacion']);
-    Route::get('/obtener', [MarcacionController::class, 'obtenerMarcaciones']);
-    Route::get('/estado', [MarcacionController::class, 'verificarEstado']);
-    Route::get('/empresa', [MarcacionController::class, 'obtenerEmpresaInfo']);
 });
 
 // Ruta de salud del sistema
