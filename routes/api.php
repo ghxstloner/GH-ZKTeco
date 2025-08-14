9777936f6a6e071d631a1c0918c999db31b1cd23
yoinerdeyhan@gmail.com <yoinerdeyhan@gmail.com>
2025-08-13 19:19:13 -0500

    ✨ [feat] marcaciones

--- routes/api.php
+++ routes/api.php
@@ -27,10 +27,16 @@ Route::group(['prefix' => '{codigo}', 'where' => ['codigo' => '[0-9]+'], 'middle
         Route::get('/cdata', [UploadProcessController::class, 'getCdata']);
         Route::post('/cdata', [UploadProcessController::class, 'postCdata']);
 
+        // --- RUTA DE REGISTRO AÑADIDA ---
+        // 3. El dispositivo se registra en el servidor. Solo necesita una respuesta OK.
+        Route::post('/registry', function (Request $request) {
+            Log::info('Dispositivo registrado exitosamente: ' . $request->input('SN'));
+            return response('OK', 200)->header('Content-Type', 'text/plain');
+        });
+
         // --- RUTAS DE COMANDOS CORREGIDAS ---
 
         // 1. El dispositivo PREGUNTA por nuevos comandos pendientes en esta ruta.
-        //    (Corregido de /devicecmd a /getrequest)
         Route::get('/getrequest', [DownloadProcessController::class, 'getRequest']);
 
         // 2. El dispositivo INFORMA el resultado de los comandos que ejecutó en esta ruta.
